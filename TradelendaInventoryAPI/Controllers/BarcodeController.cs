a4c109ea63806a499bd21289179b4812057c1e6b
Shine2n2 <kelikume@gmail.com>
2024-09-15 19:06:43 +0100

    created barcode service

---
+++ TradelendaInventoryAPI/Controllers/BarcodeController.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLogic.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TradelendaInventoryAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BarcodeController : ControllerBase
+    {
+        private readonly IBarCodeService _barcodeService;
+
+        public BarcodeController(IBarCodeService barcodeService)
+        {
+            _barcodeService = barcodeService;
+        }
+
+
+        [HttpGet("barcode")]
+        public IActionResult GenerateBarcode([FromQuery] string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return BadRequest("Text cannot be empty.");
+            }
+
+            var barcodeImage = _barcodeService.GenerateBarcode(text);
+            return File(barcodeImage, "image/png");
+        }
+
+
+        [HttpGet("qrcode")]
+        public IActionResult GenerateQRCode([FromQuery] string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return BadRequest("Text cannot be empty.");
+            }
+
+            var qrCodeImage = _barcodeService.GenerateQRCode(text);
+            return File(qrCodeImage, "image/png");
+        }
+    }
+}
